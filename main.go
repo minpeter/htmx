b0917b7495efefe327e89b21d86589082b7f1df4
Woonggi Min <minpeter@friendli.ai>
2023-12-04 17:19:47 +0900

    Refactor viewRender function and update routes in main.go

--- main.go
+++ main.go
@@ -6,32 +6,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func viewRender(c *gin.Context) {
-	var view []byte
-
-	templateName := c.Request.URL.Path
-
-	if templateName == "/" {
-		templateName = "home"
-	}
-
-	if c.GetHeader("Hx-Request") == "true" {
-		view, _ = RenderTemplates("htmx", templateName)
-	} else {
-		view, _ = RenderTemplates("main", templateName)
-	}
-	c.Data(http.StatusOK, "text/html; charset=utf-8", view)
-}
-
 func main() {
 	r := gin.Default()
 
 	r.LoadHTMLGlob("templates/components/*")
 
 	view := r.Group("/")
-	view.Use(viewRender)
-	view.GET("/", func(c *gin.Context) {})
-	view.GET("/challenge", func(c *gin.Context) {})
+	view.GET("/", func(c *gin.Context) {
+		RenderTemplates(c, gin.H{})
+	})
+
+	view.GET("/challenge", func(c *gin.Context) {
+		RenderTemplates(c, gin.H{
+			"Text": "Hello, World!",
+		})
+	})
 
 	r.GET("/new", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "create.tmpl", nil)
--- utils.go
+++ utils.go
@@ -2,41 +2,51 @@ package main
 
 import (
 	"bytes"
-	"fmt"
+	"net/http"
 	"path/filepath"
 	"text/template"
+
+	"github.com/gin-gonic/gin"
 )
 
-func RenderTemplates(mainTemplateName, pagesDirPath string) ([]byte, error) {
+func RenderTemplates(c *gin.Context, Data any) {
+
+	mainTemplateName := "main"
+
+	if c.GetHeader("Hx-Request") == "true" {
+		mainTemplateName = "htmx"
+	}
+
+	templateName := c.Request.URL.Path
+
+	if templateName == "/" {
+		templateName = "home"
+	}
+
 	// 메인 템플릿 디렉토리
 	mainTemplateDir := "templates/layouts/"
 
 	// 템플릿 생성
 	tmpl, err := template.New(mainTemplateName).ParseGlob(filepath.Join(mainTemplateDir, "*.tmpl"))
 	if err != nil {
-		return nil, err
+		return
 	}
-
-	fmt.Println("1")
 
 	// 서브 템플릿 등록
-	subTemplatePath := filepath.Join("templates/pages/", pagesDirPath+".tmpl")
+	subTemplatePath := filepath.Join("templates/pages/", templateName+".tmpl")
 	_, err = tmpl.ParseFiles(subTemplatePath)
 	if err != nil {
-		return nil, err
+		return
 	}
-
-	fmt.Println("2")
 
 	// 렌더링 결과를 저장할 버퍼 생성
 	var result bytes.Buffer
 
 	// 템플릿 실행 및 결과를 버퍼에 쓰기
-	err = tmpl.ExecuteTemplate(&result, mainTemplateName+".tmpl", nil)
+	err = tmpl.ExecuteTemplate(&result, mainTemplateName+".tmpl", Data)
 	if err != nil {
-		return nil, err
+		return
 	}
 
-	// 렌더링된 결과를 []byte로 반환
-	return result.Bytes(), nil
+	c.Data(http.StatusOK, "text/html; charset=utf-8", result.Bytes())
 }
